2a5f32acf8e476743c52f62c72f35a26ef69e2e1
Guadman98 <guadmanmen0722@gmail.com>
2024-05-20 21:26:22 -0500

    Creacion de la SPA Countries en Angular 17

---
+++ src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  templateUrl: './aboutPage.component.html',
+})
+export class AboutPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
